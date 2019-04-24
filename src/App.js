d8c3062dd68dc496ed4d97ca60cf3d1f35cc9ab1
Lauren Reilly <LaurenReillyNet@gmail.com>
2019-04-24 19:58:41 -0400

    completed sports exercise by re-using restaurants components

--- src/App.js
+++ src/App.js
@@ -3,24 +3,34 @@ import Cuisines from './components/restaurants/Cuisines';
 import Genre from './components/movies/Genre';
 import data from './restaurantData';
 import movieData from './movieData';
+import sportsData from './sportsData';
+
 import './App.css';
 
 function App() {
-  let cuisines = Object.keys(data)
+  let cuisines = Object.keys(data);
   let genres = Object.keys(movieData);
+  let sports = Object.keys(sportsData);
   return (
     <div className="App">
-      <h1>Restaurants Exercise</h1>
+      <h1 style={{color: "grey"}}>Restaurants Exercise</h1>
         {
           cuisines.map((cuisine, i) => {
             return <Cuisines key={i} cuisine={cuisine} restaurants={data[cuisine]}/>
           })
         }
         <hr/>
-        <h1>Movies Exercise</h1>
+        <h1 style={{color: "grey"}}>Movies Exercise</h1>
         {
           genres.map((genre, i) => {
             return <Genre key={i} genre={genre} data={movieData[genre]}/>
+          })
+        }
+        <hr/>
+        <h1 style={{color: "grey"}}>Sports Exercise (Reusing restaurants components because I can)</h1>
+        {
+          sports.map((cuisine, i) => {
+            return <Cuisines key={i} cuisine={cuisine} restaurants={sportsData[cuisine]}/>
           })
         }
     </div>
